8d4de6cf97f63c1d79f97adf4f230f007022db8c
Vivian Marino <marino6733784@gmail.com>
2023-10-10 15:45:47 -0400

    tarea crud y json objects

--- src/main/java/jsonMethods/Comparator.java
+++ src/main/java/jsonMethods/Comparator.java
@@ -6,15 +6,31 @@ import java.util.Arrays;
 public class Comparator {
     public static boolean personalizedEquals(JSONObject expected, JSONObject actual){
         String [] keys = JSONObject.getNames(expected);
-        if(Arrays.equals(keys, JSONObject.getNames(actual))){
+        boolean result = true;
+        if(Arrays.equals(keys, JSONObject.getNames(actual)) && keys!=null){
+            System.out.println("----------------------------------------------");
             for( String key : keys){
-                if(!(expected.get(key).equals(actual.get(key)) | expected.get(key).equals("ignore"))){
-                    return false;
+                boolean comparison = (expected.get(key).equals(actual.get(key)) | expected.get(key).equals("ignore"));
+                result = result && comparison;
+                if(!comparison){
+                    System.out.println("Expected "+key+": "+expected.get(key)+", Actual "+key+": "+actual.get(key));
                 };
             }
-            return true;
-        } else{
-            return false;
+        }else if(keys==null && JSONObject.getNames(actual)==null){
+            System.out.println("----------------------------------------------");
+            System.out.println("Los objetos estan vacios");
+        }else if(keys==null && JSONObject.getNames(actual)!=null){
+            System.out.println("----------------------------------------------");
+            System.out.println("El objeto json Expected esta vacio");
+            result=false;
         }
+        else{
+            System.out.println("----------------------------------------------");
+            System.out.println("Los objetos no tienen los mismos parametros");
+            System.out.println("Expected: "+expected.toString());
+            System.out.println("Actual: "+actual.toString());
+            result = false;
+        }
+        return result;
     }
 }
--- src/test/java/jsonMethods/ComparatorTest.java
+++ src/test/java/jsonMethods/ComparatorTest.java
@@ -11,10 +11,49 @@ public class ComparatorTest {
                 "{\"city\":\"ignore\",\"name\":\"jon doe\",\"age\":\"ignore\"}"
         );
         JSONObject jo2 = new JSONObject(
+                "{\"city\":\"chicago\",\"name\":\"jon doe\",\"age\":\"23\"}"
+        );
+        boolean actual = Comparator.personalizedEquals(jo,jo2);
+        boolean expected = true;
+        Assertions.assertEquals(expected, actual, "ERROR: el metodo no los compara correctamente");
+    }
+
+    @Test
+    public void verifyJSONObjectNotEquals(){
+        JSONObject jo = new JSONObject(
+                "{\"city\":\"ignore\",\"name\":\"jon doe\",\"age\":\"ignore\"}"
+        );
+        JSONObject jo2 = new JSONObject(
                 "{\"city\":\"chicago\",\"name\":\"jon poe\",\"age\":\"23\"}"
         );
         boolean actual = Comparator.personalizedEquals(jo,jo2);
         boolean expected = false;
+        Assertions.assertEquals(expected, actual, "ERROR: el metodo no los compara correctamente");
+    }
+
+    @Test
+    public void verifyJSONObjectNotSameParameters(){
+        JSONObject jo = new JSONObject(
+                "{ }"
+        );
+        JSONObject jo2 = new JSONObject(
+                "{\"city\":\"chicago\",\"name\":\"jon doe\",\"age\":\"23\"}"
+        );
+        boolean actual = Comparator.personalizedEquals(jo,jo2);
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual, "ERROR: el metodo no los compara correctamente");
+    }
+
+    @Test
+    public void verifyJSONObjectBothEmpty(){
+        JSONObject jo = new JSONObject(
+                "{ }"
+        );
+        JSONObject jo2 = new JSONObject(
+                "{ }"
+        );
+        boolean actual = Comparator.personalizedEquals(jo,jo2);
+        boolean expected = true;
         Assertions.assertEquals(expected, actual, "ERROR: el metodo no los compara correctamente");
     }
 }
